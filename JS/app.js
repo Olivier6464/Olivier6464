b1647b59c71b2c9e778d58d91f5ce96bbd287e66
Olivier Ernst <olivier.ernst0411@orange.fr>
2021-12-01 18:40:35 +0100

    mise en forme

--- JS/app.js
+++ JS/app.js
@@ -438,3 +438,10 @@ function fdate() {
 }
 
 fdate();
+
+function rmuglyspaces(id) {
+  ch=document.getElementById(id).value
+  ch = ch.replace(/^\s*/gm, '')
+  ch = ch.replace(/  +/g, ' ')
+  document.getElementById(id).value = ch
+}
