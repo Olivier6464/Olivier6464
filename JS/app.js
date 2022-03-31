044de5ca363bb9eb7cb34a90bf042dcc2839f396
Olivier Ernst <olivier.ernst0411@orange.fr>
2022-03-31 22:20:27 +0200

    supression des lignes vides

--- JS/app.js
+++ JS/app.js
@@ -50,7 +50,7 @@ function rmNastyChars(ch){
   ch = ch.replaceAll("'", " ");
   ch = ch.replaceAll(/[:()"]/g, ""); 
   // lignes vides en double
-  //ch = ch.replaceAll(/\n\n+/g,"\n\n");     
+  ch = ch.replaceAll(/\n\n+/g,"\n\n");     
   return ch;
 }
 
