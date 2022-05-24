ad08ad51b98579f12ab5ed5c13db0beab59e86d4
Olivier Ernst <olivier.ernst0411@orange.fr>
2022-05-24 23:13:01 +0200

    rmNastyChars

--- JS/app.js
+++ JS/app.js
@@ -49,14 +49,15 @@ const spacer = {
 function rmNastyChars(ch) {
   ch = ch.replaceAll("'", " ");
   ch = ch.replaceAll(/[:()"]/g, "");
-  ch = ch.replaceAll(/(^ +)/gm, '');
+  ch = ch.replaceAll(/(^ +)/gm, "");
   ch = ch.replaceAll(/^> +/gm, "- ");
   ch = ch.replaceAll(/^>./gm, "- ");
   ch = ch.replaceAll(/^(=> +)/gm, "- ");
   ch = ch.replaceAll(/^(=>.)/gm, "- ");
   ch = ch.replaceAll(/^(\* +)/gm, "- ");
   ch = ch.replaceAll(/^(\*.)/gm, "- ");
-  ch = ch.replaceAll(/(?!^)-/gm, '_');
+  ch = ch.replaceAll(/(?!^)-/gm, "_");
+  ch = ch.replaceAll(/(\/+)/gm, "//");
   // lignes vides en double
   ch = ch.replaceAll(/\n\n+/g, "\n\n");
   return ch;
